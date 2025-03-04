0c302fb5b4905677810c2bca68923b00f63e6024
Zashhive <susanna.i.arnold@proton.me>
2025-03-05 06:19:36 +0900

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+// JavaScript for mobile menu toggle
+const mobileToggle = document.querySelector('.mobile-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+mobileToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
